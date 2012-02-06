f4f6e58a69f1e36f286bc9643b4ff8a6f9e11155
Kapil Gahlawat <kapil@quadnode.com>
2012-02-06 15:19:06 +0530

    background color modification

--- gen/com/quadnode/activities/R.java
+++ gen/com/quadnode/activities/R.java
@@ -13,6 +13,7 @@ public final class R {
     public static final class color {
         public static final int blue1=0x7f040000;
         public static final int blue2=0x7f040001;
+        public static final int grey=0x7f040002;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
